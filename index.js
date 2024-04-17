ec4f20fbef055049aa48bfcf68253d1b5a5689ee
Yamileth Reveca <yamilethcaldera@gmail.com>
2024-04-16 22:06:03 -0400

    Calcular planes v.1

---
+++ index.js
@@ -0,0 +1,35 @@
+
+
+const montoUsuario = document.getElementById('montoUsuario');
+const optionesPlanes = document.getElementById('optionesPlanes');
+let comisionInfo = document.getElementById('comisionInfo');
+const calcularComisionBtn = document.getElementById('calcularComision');
+
+optionesPlanes.addEventListener('click', function() {
+  const valorProducto = parseFloat(montoUsuario.value);
+  const planSeleccionado = parseFloat(optionesPlanes.value);
+  const comision = valorProducto * (planSeleccionado / 100);
+  const iva = comision * 0.19; // Calculando el IVA del 19%
+  const totalAPagar = Math.round(comision + iva);
+
+  comisionInfo.innerHTML="$"+totalAPagar.toLocaleString();
+
+ 
+  console.log(valorProducto)
+  console.log(planSeleccionado)
+  console.log(comision)
+  console.log(iva)
+  console.log(totalAPagar)
+  console.log(comisionInfo)
+})
+function mostrarPlanSeleccionado() {
+  const optionesPlanes = document.getElementById('optionesPlanes');
+  const planSeleccionado = document.getElementById('planSeleccionado');
+
+  // Obtener el valor y el texto del plan seleccionado
+  const planValue = optionesPlanes.options[optionesPlanes.selectedIndex].value;
+  const planText = optionesPlanes.options[optionesPlanes.selectedIndex].text;
+
+  // Mostrar el número y la descripción del plan seleccionado dentro de un círculo
+  planSeleccionado.innerHTML = `<i class="bi bi-${optionesPlanes.selectedIndex + 1}-circle"></i> ${planText} ${planValue}% + IVA`;
+}
